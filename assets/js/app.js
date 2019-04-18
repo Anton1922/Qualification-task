b6a70fd8a046fd5e1c2bdfae1c512ca98a455724
Anton Fedorov <anton.pzkt@gmail.com>
2019-04-19 01:57:41 +0200

    added events for navigation by click

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,100 +1,39 @@
-import Scrollbar from 'smooth-scrollbar';
-import '@fancyapps/fancybox';
-import 'select2/dist/js/select2.full';
+$(document).ready(function(){
+    var $jsArrowBack = $("#js-arrow-back");
+    var $jsArrowForward = $("#js-arrow-forward");
+    var $slide = $(".slide");
+    var $jsNavNumLeft = $("#js-navigation-number-left");
+    var i = 1;
 
-let app = {
-    init() {
-        func.jsScale();
-        func.jsSelect();
-    },
-    load() {
+    $jsArrowForward.click(function(){
+        $("#slide"+i).fadeOut(1);
+        $("#js-dot"+i).css("background-color", "#11264A");
+        //$jsNavNumLeft.html(i);
+        i ++;
+        $("#slide"+i).fadeIn(1);
+        $("#slide"+i).css("display", "flex");
+        $("#js-dot"+i).css("background-color", "#E8C48A");
+        $jsNavNumLeft.html("0"+i);
 
-    },
-    resize() {
-        func.jsScale();
-    },
-    scroll() {
-
-    },
-    minWindowWidth: 1920
-    //minWindowWidth: 1430
-};
-
-let func = {
-    jsScale() {
-        let currentWidth = document.documentElement.clientWidth;
-        let $el = $('body');
-        let $elContent = $('.wrapper');
-        let $elScaleMinHeight = $('.js-scale-min-height');
-
-        if (currentWidth < app.minWindowWidth) {
-
-            /*$el.css({
-                'transform': `scale(${currentWidth / app.minWindowWidth})`
-            });
-
-            $elContent.css({
-                'max-width': $elContent[0].scrollWidth * currentWidth / app.minWindowWidth,
-                //'overflow': 'hidden'
-            });*/
-
-            //$('html, body').css('font-size', `${(currentWidth / app.minWindowWidth) * 100}%`);
-            $('html, body').css('font-size', `${(currentWidth / app.minWindowWidth) * 17}px`);
-
-            /*$elScaleMinHeight.css({
-                'min-height': document.documentElement.scrollHeight * app.minWindowWidth / currentWidth
-            });*/
-        } else {
-
-            $('html, body').css('font-size', '');
-
-            $el.css({
-                'transform': ''
-            });
-
-            $elContent.css({
-                'max-height': '',
-                'overflow': ''
-            });
-
-            $elScaleMinHeight.css({
-                'min-height': ''
-            });
-        }
-    },
-    jsSelect() {
-        $('.js-select').select2({
-            minimumResultsForSearch: -1,
-            containerCssClass: 'raif-select__select',
-            dropdownCssClass: 'raif-select__results',
-            width: '100%',
-            theme: 'raif-select'
-        });
-    }
-};
-
-$(function () {
-
-    app.init();
-
-    if (navigator.userAgent.indexOf('MSIE') !== -1 || navigator.appVersion.indexOf('Trident/') > 0) {
-        var evt = document.createEvent('UIEvents');
-        evt.initUIEvent('resize', true, false, window, 0);
-        window.dispatchEvent(evt);
-    } else {
-        window.dispatchEvent(new Event('resize'));
-    }
-
-    $(window).on('load', function () {
-        app.load();
     });
 
-    $(window, document).on('resize', function () {
-        app.resize();
+    $jsArrowBack.click(function(){
+        $("#slide"+i).fadeOut(1);
+        $("#js-dot"+i).css("background-color", "#11264A");
+        i --;
+        $("#slide"+i).fadeIn(1);
+        $("#slide"+i).css("display", "flex");
+        $("#js-dot"+i).css("background-color", "#E8C48A");
+        $jsNavNumLeft.html("0"+i);
+
+
+
     });
 
-    $(window).on('scroll', function () {
-        app.scroll();
-    });
+
+
+
+
+
 
 });
