ac6004c9befbd8f25a928baab0499d8c3d78edca
Anton Fedorov <anton.pzkt@gmail.com>
2019-04-22 02:05:17 +0200

    owl carousel was initialize and small changes for it

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
+
     var $jsArrowBack = $("#js-arrow-back");
     var $jsArrowForward = $("#js-arrow-forward");
     var $slide = $(".slide");
     var $jsNavNumLeft = $("#js-navigation-number-left");
+
     var i = 1;
 
     $jsArrowForward.click(function(){
@@ -14,6 +16,12 @@ $(document).ready(function(){
         $("#slide"+i).css("display", "flex");
         $("#js-dot"+i).css("background-color", "#E8C48A");
         $jsNavNumLeft.html("0"+i);
+        $(".navigation__link").mouseenter(function(){
+            $(this).css("background-color", "#E8C48A");
+        });
+        $(".navigation__link").mouseleave(function(){
+            $(this).css("background-color", "#11264A");
+        });
 
     });
 
@@ -25,15 +33,52 @@ $(document).ready(function(){
         $("#slide"+i).css("display", "flex");
         $("#js-dot"+i).css("background-color", "#E8C48A");
         $jsNavNumLeft.html("0"+i);
+    });
+});
 
 
+$(document).ready(function(){
+    var $jsSelectYear = $(".js-select-year");
+
+    /*$jsSelectYear.select2({
+        placeholder: "Выберите год"
+
+    });*/
+
+    $jsSelectYear.SumoSelect({
+        placeholder: "Выберите год"
 
     });
 
-
-
-
-
-
-
 });
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    nav: true,
+    mouseDrag: true,
+    items: 1,
+    dotsEach: true
+  });
+
+  $(".owl-prev span").html("");
+  $(".owl-next span").html("");
+
+  /*if ($(".owl-dot").hasClass("active")) {
+    $(this).css("background-color", "#E8C48A");
+  }*/
+});
+
+/*$(document).ready(function(){
+  var $containerSlider = $(".container-slider");
+
+  $containerSlider.slick({
+    //slidesToShow: 3,
+    //slidesToScroll: 3
+
+
+
+
+  });
+
+
+});*/
